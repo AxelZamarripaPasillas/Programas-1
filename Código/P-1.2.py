d19c4bc557ddc0c1f2f9b52be81ad939e1310dea
AxelZamarripaPasillas <axelzamarripapasillas@gmail.com>
2024-11-22 20:25:48 -0600

    Update P-1.2.py

--- Código/P-1.2.py
+++ Código/P-1.2.py
@@ -1,2 +1,20 @@
-#TIPOS DE DATOS
+# TIPOS DE DATOS
 
+#Alfanumérico (String)
+nombre = "Juan"
+print(nombre)
+
+#Enteros
+edad = 30
+print(edad)
+
+#Flotantes
+altura = 1.75
+print(altura)
+
+#Binarios
+mensaje = b'Hola, mundo!'
+print(mensaje)
+
+#Listas
+frutas = ["Manzana", "Pera", "Mango"]
